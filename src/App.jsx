90e7e42a68386677ae3df418d7f8a9fe001d3c59
Sohan <sohansharad@gmail.com>
2025-10-18 17:41:39 +0100

    changed layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,17 @@ import React from 'react'
 import './App.css'
 import DataSection from './Components/DataSection'
 import AISection from './Components/AISection'
-import { PanelGroup } from 'react-resizable-panels'
-import { Panel } from 'react-resizable-panels'
-import { PanelResizeHandle } from 'react-resizable-panels'
 
 const App = () => {
     return (
         <div id='AppDiv'>
-            <PanelGroup direction="horizontal">
-                <Panel minSize={30}>
-                    <DataSection />
-                </Panel>
-                <PanelResizeHandle className={"ResizeHandleOuter"}>
-                    <div className={"ResizeHandleInner"}>   
-                        <svg className={"Icon"} viewBox="0 0 24 24">
-                            <path
-                                fill="currentColor"
-                                d="M8,18H11V15H2V13H22V15H13V18H16L12,22L8,18M12,2L8,6H11V9H2V11H22V9H13V6H16L12,2Z"
-                            />
-                        </svg>
-                    </div>
-                </PanelResizeHandle>
-                <Panel minSize={30}>
-                    <AISection />
-                </Panel>
-            </PanelGroup>
+            <div id='DataSectionDiv'>
+                <DataSection />
+            </div>
+
+            <div id='AISectionDiv'>
+                <AISection />
+            </div>
         </div>
     )
 }
