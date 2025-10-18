5865158f01209009f266316d3ecf73c1c55c3844
Jer-Dan <jeremy.daniel.jd@protonmail.com>
2025-10-18 22:43:37 +0100

    Added variables for calcBalance

--- src/Components/DataSection.jsx
+++ src/Components/DataSection.jsx
@@ -5,6 +5,18 @@ import '../Styles/DataSection.css'
 import Dropdown from 'react-bootstrap/Dropdown';
 import PersonalForm from './PersonalForm';
 
+function calcBalance() {
+  const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  let month = new Date().getMonth();
+  let day = new Date().getDay();
+
+  let lastDay = localStorage.getItem('lastDay') || 0;
+  let monthBalance = localStorage.getItem('monthBalance') || 0;
+  let monthCardInfo = JSON.parse(localStorage.getItem('monthCardInfo')) || {};
+  let paymentsInMonth = localStorage.getItem('paymentsInMonth') || 0;
+  let pastMonthData = JSON.parse(localStorage.getItem('pastMonthData')) || [];
+}
+
 function DataSection() {
 
     const [FormType, setFormType] = useState("")
