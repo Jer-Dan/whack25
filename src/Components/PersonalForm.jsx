292ba1747fda97f860f5659539bad802c4f4827f
Jer-Dan <jeremy.daniel.jd@protonmail.com>
2025-10-18 21:08:33 +0100

    Info updated after filling in the form

--- src/Components/PersonalForm.jsx
+++ src/Components/PersonalForm.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { Button } from 'react-bootstrap';
 import AddPersonModal from './AddPersonModal';
 
+const credit_providers = ["Experian", "Equifax", "TransUnion"];
+
 function PersonalForm() {
     // state to manage if personal info is already added
     const [infoAdded, setInfoAdded] = useState(false);
@@ -24,13 +26,17 @@ function PersonalForm() {
         {!infoAdded && (
             <Button variant="primary" onClick={() => setShowModal(true)}>Add Personal Information</Button>
         )}
-        {showModal && <AddPersonModal show={showModal} onHide={() => setShowModal(false)} />}
+        {showModal && <AddPersonModal show={showModal} onHide={() => {
+            setShowModal(false);
+            forceUpdate();
+          }
+        />}
 
         <div className="mt-3">
             <h3>Personal Information:</h3>
             <p><strong>Name:</strong> {personalInfo.name || 'N/A'}</p>
             <p><strong>Income (Yearly):</strong> {personalInfo.income || 'N/A'}</p>
-            <p><strong>Credit Score Provider:</strong> {personalInfo.creditScoreProvider || 'N/A'}</p>
+            <p><strong>Credit Score Provider:</strong> {credit_providers[personalInfo.providerNumber] || 'N/A'}</p>
             <p><strong>Credit Score:</strong> {personalInfo.creditScore || 'N/A'}</p>
         </div>
     </div>
